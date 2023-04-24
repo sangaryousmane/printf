ab5050111156b82ca31c434a09c54f870e28ddf2
Fatima Slimani <fatimaslimani0@icloud.com>
2023-04-24 13:55:24 +0400

    feat: mv main.c

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <limits.h>
-#include "../main.h"
+#include "main.h"
 
 /**
  * main - Entry point
@@ -18,27 +18,37 @@ int main(void)
 	len2 = printf("Let's try to printf a simple sentence.\n");
 	ui = (unsigned int)INT_MAX + 1024;
 	addr = (void *)0x7ffe637541f0;
-	_printf("Length:[%d, %i]\n", len, len);
-	printf("Length:[%d, %i]\n", len2, len2);
+	len = _printf("Length:[%d, %i]\n", len, len);
+	len2 = printf("Length:[%d, %i]\n", len2, len2);
+	_printf("Length:[%d = %d]\n\n", len, len2);
+
 	_printf("Negative:[%d]\n", -762534);
 	printf("Negative:[%d]\n", -762534);
+
 	_printf("Unsigned:[%u]\n", ui);
 	printf("Unsigned:[%u]\n", ui);
 	_printf("Unsigned octal:[%o]\n", ui);
 	printf("Unsigned octal:[%o]\n", ui);
 	_printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
 	printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
-	_printf("Character:[%c]\n", 'H');
-	printf("Character:[%c]\n", 'H');
-	_printf("String:[%s]\n", "I am a string !");
-	printf("String:[%s]\n", "I am a string !");
+
+	len = _printf("Character:[%c]\n", 'H');
+	len2 = printf("Character:[%c]\n", 'H');
+	_printf("Length:[%d = %d]\n\n", len, len2);
+
+	len = _printf("String:[%s]\n", "I am a string !");
+	len2 = printf("String:[%s]\n", "I am a string !");
+	_printf("Length:[%d = %d]\n\n", len, len2);
+
 	_printf("Address:[%p]\n", addr);
 	printf("Address:[%p]\n", addr);
 	len = _printf("Percent:[%%]\n");
 	len2 = printf("Percent:[%%]\n");
-	_printf("Len:[%d]\n", len);
-	printf("Len:[%d]\n", len2);
-	_printf("Unknown:[%r]\n");
-	printf("Unknown:[%r]\n");
+	_printf("Length:[%d = %d]\n\n", len, len2);
+
+	len = _printf("Unknown:[%r]\n");
+	len2 = printf("Unknown:[%r]\n");
+	_printf("Length:[%d = %d]\n\n", len, len2);
+
 	return (0);
 }
--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
-gcc -Wall -Wextra -Werror -pedantic -std=gnu89 -Wno-format *.c ./test/main.c
+gcc -Wall -Wextra -Werror -pedantic -std=gnu89 -Wno-format *.c
 ./a.out
 rm a.out 
