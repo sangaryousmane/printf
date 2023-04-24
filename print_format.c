1451d93c0cab0dce4f83fe165d4b42db0dfc9f63
Fatima Slimani <fatimaslimani0@icloud.com>
2023-04-24 16:58:13 +0400

    format(printf): format file

--- print_format.c
+++ print_format.c
@@ -9,29 +9,29 @@
  */
 int parse_signed_number(const char *str, int *length)
 {
-    int sign = 1;
-    int value = 0;
+	int sign = 1;
+	int value = 0;
 
-    *length = 0;
+	*length = 0;
 
-    /* (+|-)* */
-    while (str[*length] == '+' || str[*length] == '-')
-    {
-        if (str[*length] == '-')
-        {
-            sign *= -1;
-        }
-        *length = *length + 1;
-    }
+	/* (+|-)* */
+	while (str[*length] == '+' || str[*length] == '-')
+	{
+		if (str[*length] == '-')
+		{
+			sign *= -1;
+		}
+		*length = *length + 1;
+	}
 
-    /* (0-9)* */
-    while (str[*length] >= '0' && str[*length] <= '9')
-    {
-        value = value * 10 + str[*length] - '0';
-        *length = *length + 1;
-    }
+	/* (0-9)* */
+	while (str[*length] >= '0' && str[*length] <= '9')
+	{
+		value = value * 10 + str[*length] - '0';
+		*length = *length + 1;
+	}
 
-    return (sign * value);
+	return (sign * value);
 }
 
 /**
@@ -44,31 +44,31 @@ int parse_signed_number(const char *str, int *length)
  */
 int print_formatted(const char *current, va_list ap, int *token_length)
 {
-    int length = 0;
+	int length = 0;
 
-    /* (+|-)*(0-9)* */
-    if ((*current >= '0' && *current <= '9') || *current == '+' || *current == '-')
-    {
-        parse_signed_number(current, &length);
-    }
-    *token_length = length + 1;
+	/* (+|-)*(0-9)* */
+	if ((*current >= '0' && *current <= '9') || *current == '+' || *current == '-')
+	{
+		parse_signed_number(current, &length);
+	}
+	*token_length = length + 1;
 
-    switch (*current)
-    {
-    case 'c':
+	switch (*current)
+	{
+	case 'c':
 
-        return (print_char(ap));
-    case 's':
-        return (print_string(ap));
-    case 'd':
-        return (print_int(ap));
-    case 'i':
-        return (print_int(ap));
-    case '%':
-        return (print_unknown('%'));
+		return (print_char(ap));
+	case 's':
+		return (print_string(ap));
+	case 'd':
+		return (print_int(ap));
+	case 'i':
+		return (print_int(ap));
+	case '%':
+		return (print_unknown('%'));
 
-    default:
-        *token_length = 0;
-        return (0);
-    }
+	default:
+		*token_length = 0;
+		return (0);
+	}
 }
