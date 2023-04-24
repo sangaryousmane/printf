0b5a9ca6799891b3f6b43f007669315c60226bc6
Fatima Slimani <fatimaslimani0@icloud.com>
2023-04-24 13:04:12 +0400

    chore(printf): format files

--- test/main.c
+++ test/main.c
@@ -3,10 +3,10 @@
 
 int main(void)
 {
-    int len, len2;
-    len = printf("age: %c \n", 102);
-    len2 = _printf("age: %c \n", 102);
-    printf("%d = %d", len, len2);
+	int len, len2;
+	len = printf("age: %c \n", 102);
+	len2 = _printf("age: %c \n", 102);
+	printf("%d = %d", len, len2);
 
-    return 0;
+	return 0;
 }
