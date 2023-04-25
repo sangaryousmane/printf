82784b60ab10755c547c956480bce7eb325df3bc
Fatima Slimani <fatimaslimani0@icloud.com>
2023-04-25 19:24:21 +0400

    format main.h

--- main.h
+++ main.h
@@ -37,7 +37,9 @@ typedef struct fmt fmt_t;
 
 int _printf(const char *format, ...);
 int handle_print(const char *fmt, int *i,
-				 va_list list, char buffer[], int flags, int width, int precision, int size);
+				 va_list list,
+				 char buffer[],
+				 int flags, int width, int precision, int size);
 
 int print_char(va_list types, char buffer[],
 			   int flags, int width, int precision, int size);
@@ -60,7 +62,9 @@ int print_hexa_upper(va_list types, char buffer[],
 					 int flags, int width, int precision, int size);
 
 int print_hexa(va_list types, char map_to[],
-			   char buffer[], int flags, char flag_ch, int width, int precision, int size);
+			   char buffer[],
+			   int flags,
+			   char flag_ch, int width, int precision, int size);
 
 int print_non_printable(va_list types, char buffer[],
 						int flags, int width, int precision, int size);
