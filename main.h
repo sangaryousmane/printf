74e767005c776b2b3de233658c6c6aea8312869c
sangaryousmane <sangaryousmane@gmail.com>
2023-04-25 00:29:51 -0700

    Add more_func.c and main.h

--- main.h
+++ main.h
@@ -40,4 +40,6 @@ int get_flags(const char *format, int *i);
 int get_size(const char *format, int *i);
 int get_width(const char *format, int *i, va_list list);
 int get_precision(const char *format, int *i, va_list list);
+int print_binary(va_list types, char buffer[],
+int flags, int width, int precision, int size);
 #endif
